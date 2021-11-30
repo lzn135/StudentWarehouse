2d7e9623316dfe0279d73c19be70ff51aca44296
竜胆御言 <67625251+lzn135@users.noreply.github.com>
2021-11-30 18:26:18 +0800

    Delete 0921 directory

--- 0921/src/org/lanqiao/lanbda/Lambda.java
+++
@@ -1,5 +0,0 @@
-package org.lanqiao.lanbda;
-
-public class Lambda (String str,MyFunction mf){
-
-		}
--- 0921/src/org/lanqiao/lanbda/MyFunction.java
+++
@@ -1,6 +0,0 @@
-package org.lanqiao.lanbda;
-
-
-public interface MyFunction {
-	@FunctionalInterface
-}
