1a5f70cc5941b037c591693e1a95d26e345a0cc4
竜胆御言 <67625251+lzn135@users.noreply.github.com>
2021-11-30 18:44:02 +0800

    Delete 1111 directory

--- 1111/src/org/lanqiao/dao/Userdao.java
+++
@@ -1,9 +0,0 @@
-package org.lanqiao.dao;
-
-import org.lanqiao.User;
-
-import java.sql.SQLException;
-
-public interface Userdao {
-	public User findUserByUsernameAndPassword(User user) throws SQLException;
-}
