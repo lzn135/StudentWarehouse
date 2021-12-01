6126fe5eb421b9b4e4124546994a9719b7839d61
竜胆御言 <67625251+lzn135@users.noreply.github.com>
2021-12-01 14:42:51 +0800

    Delete travel-master directory

--- travel-master/src/main/java/org/lanqiao/mapper/AdminUserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.lanqiao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.lanqiao.pojo.AdminUser;
-
-public interface AdminUserMapper  extends BaseMapper<AdminUser> {
-//	AdminUser login(AdminUser adminUser);
-
-}
--- travel-master/src/main/java/org/lanqiao/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.lanqiao.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.lanqiao.pojo.User;
-
-public interface UserService extends IService<User> {
-}
